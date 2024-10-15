8aec1a7421d13c84e1c76f2a51585ff8f9c87936
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-15 23:11:19 +0600

    index

---
+++ src/shared/index.js
@@ -0,0 +1,28 @@
+export const pcture = [
+    {
+      src: "../Images/Rectangle 42010.png",
+      alt: "Partner 1",
+      title:"Whitechapel Rd."
+    },
+    {
+      src: "../Images/Group 1171276280.png",
+      alt: "Partner 2",
+       title:"Whitechapel Rd."
+    },
+    {
+      src: "../Images/Group 1171276280 (1).png",
+      alt: "Partner 3",
+      title:"Tulare County"
+    },
+    {
+      src: "../Images/Group 1171276280 (2).png",
+      alt: "Partner 4",
+       title:"Tulare County"
+    },
+    {
+      src: "../Images/Group 1171276280 (3).png",
+      alt: "Partner 5",
+       title:"Tulare County"
+    },
+   
+  ];
