2ba490af1f15d10e8ff220f85c8c5e992f9edb8e
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-16 00:38:18 +0600

    incident

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,3 +1,4 @@
+
 export const pcture = [
     {
       src: "../Images/Rectangle 42010.png",
@@ -24,5 +25,97 @@ export const pcture = [
       alt: "Partner 5",
        title:"Tulare County"
     },
+   
+  ];
+export const incident = [
+    {
+      src: "../Images/fi_67855.png",
+      alt: "Partner 1",
+      title:"Avalanche"
+    },
+    {
+      src: "../Images/fi_8931510.png",
+      alt: "Partner 2",
+       title:"Biological"
+    },
+    {
+      src: "../Images/fi_3496215.png",
+      alt: "Partner 3",
+      title:"Blizzard"
+    },
+    {
+      src: "../Images/fi_2529995.png",
+      alt: "Partner 4",
+       title:"Cold/Freezing"
+    },
+    {
+      src: "../Images/fi_533057.png",
+      alt: "Partner 5",
+       title:"Drought"
+    },
+   
+    {
+      src: "../Images/fi_1818265.png",
+      alt: "Partner 2",
+       title:"Earthquake"
+    },
+    {
+      src: "../Images/fi_106063.png",
+      alt: "Partner 3",
+      title:"Flooding"
+    },
+    {
+      src: "../Images/fi_66275.png",
+      alt: "Partner 4",
+       title:"Heat Wave"
+    },
+    {
+      src: "../Images/fi_6157591.png",
+      alt: "Partner 5",
+       title:"Hail"
+    },
+    {
+      src: "../Images/fi_9502782.png",
+      alt: "Partner 4",
+       title:"Tulare County"
+    },
+    {
+      src: "../Images/fi_2165881.png",
+      alt: "Partner 5",
+       title:"Lightning"
+    },
+   
+    {
+      src: "../Images/fi_15330449.png",
+      alt: "Partner 2",
+       title:"Man Made"
+    },
+    {
+      src: "../Images/fi_15330449 (1).png",
+      alt: "Partner 3",
+      title:"Mudslide"
+    },
+    {
+      src: "../Images/fi_3682921.png",
+      alt: "Partner 4",
+       title:"Severe Storm"
+    },
+    {
+      src: "../Images/fi_533077.png",
+      alt: "Partner 5",
+       title:"Strong Wind"
+    },
+   
+    {
+      src: "../Images/fi_10567288.png",
+      alt: "Partner 3",
+      title:"Volcanic Eruption"
+    },
+    {
+      src: "../Images/fi_3661746.png",
+      alt: "Partner 4",
+       title:"Tsunami"
+    },
+   
    
   ];
